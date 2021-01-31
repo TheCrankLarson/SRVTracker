00da2c1f66774105a5b33708df51c0bfb3f07c97
TheCrankLarson <68806777+TheCrankLarson@users.noreply.github.com>
2021-01-31 22:10:58 +0000

    Implemented equator route generation

--- EDTracking/RouteGenerator.cs
+++ EDTracking/RouteGenerator.cs
@@ -11,6 +11,21 @@ namespace EDTracking
         public static List<EDWaypoint> Equator(double PlanetaryRadius, int WaypointSeparationDistance, double StartLongitude = 0)
         {
             List<EDWaypoint> waypoints = new List<EDWaypoint>();
+            int numberOfWaypoints = Convert.ToInt32(Circumference(PlanetaryRadius) / Convert.ToDouble(WaypointSeparationDistance));
+            if (numberOfWaypoints > 500)
+                return null;
+
+            double anglePerWaypoint = 360/(double)numberOfWaypoints;
+            int waypointRadius = WaypointSeparationDistance / 2;
+            if (waypointRadius > 1000)
+                waypointRadius = 1000;
+
+            for (int i = 0; i < numberOfWaypoints; i++)
+            {
+                double thisLongitude = StartLongitude + (anglePerWaypoint * i);
+                EDLocation thisLocation = new EDLocation(0, thisLongitude, 0, PlanetaryRadius);
+                waypoints.Add(new EDWaypoint(thisLocation, DateTime.Now, waypointRadius));
+            }
             return waypoints;
         }
 
@@ -18,6 +33,12 @@ namespace EDTracking
         {
             List<EDWaypoint> waypoints = new List<EDWaypoint>();
             return waypoints;
+        }
+
+        public static double Circumference(double radius)
+        {
+            // Returns the circumference of the circle
+            return (radius * radius) * Math.PI;
         }
     }
 }
